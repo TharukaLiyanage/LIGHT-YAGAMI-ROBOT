cbd08464568b20de890193c3e4d3cdecb6760662
LEGEND_OP <84610766+LEGEND-Xxxxx@users.noreply.github.com>
2021-06-21 15:04:40 +0530

    Update and rename LightYagami.py to igris.py

--- igris.py
+++ igris.py
@@ -2,7 +2,7 @@ from telethon import events
 from LightYagami import telethn
 
 """Triggers start command in pm and in groupchats"""
-def LightYagami(**args):
+def igrisbot(**args):
     """New message."""
     pattern = args.get('pattern', None)
     r_pattern = r'^[/!]'
