8881d516c5ddce64f9fc30d9f3c0c8d342d23a27
LEGEND_OP <84610766+LEGEND-Xxxxx@users.noreply.github.com>
2021-06-28 21:53:02 +0530

    Update logomaker.py

--- LightYagami/modules/logomaker.py
+++ LightYagami/modules/logomaker.py
@@ -21,13 +21,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./MashaRoBot/resources/blackbg.jpg')
+    img = Image.open('./LightYagami/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./MashaRoBot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype("./LightYagami/resources/Chopsic.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -61,13 +61,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./MashaRoBot/resources/blackbg.jpg')
+    img = Image.open('./LightYagami/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./MashaRoBot/resources/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./LightYagami/resources/Maghrib.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
