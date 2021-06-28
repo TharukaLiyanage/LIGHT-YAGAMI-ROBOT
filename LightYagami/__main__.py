fc39fd1f43983cd0517ba946b3edb679f53e4dab
LEGEND_OP <84610766+LEGEND-Xxxxx@users.noreply.github.com>
2021-06-28 19:37:29 +0530

    Update __main__.py

--- LightYagami/__main__.py
+++ LightYagami/__main__.py
@@ -219,7 +219,7 @@ def start(update: Update, context: CallbackContext):
                     ]]))
     else:
         update.effective_message.reply_video(
-                LIGHT_IMG, caption=f"============== 𝐇𝐞𝐥𝐥𝐨 𝐈'𝐌 𝐊𝐢𝐫𝐚 😈 𝐋𝐞𝐭'𝐬 𝐊𝐢𝐥𝐥 𝐁𝐚𝐝 𝐏𝐞𝐨𝐩𝐥𝐞𝐬\n𝐁𝐲 𝐖𝐫𝐢𝐭𝐢𝐧𝐠 𝐓𝐡𝐨𝐬𝐞 𝐍𝐚𝐦𝐞 𝐎𝐧 𝐃𝐞𝐚𝐭𝐡 𝐍𝐨𝐭𝐞 =============")      
+                LIGHT_IMG, caption=f"======❤ 𝐇𝐞𝐥𝐥𝐨 𝐈'𝐌 𝐊𝐢𝐫𝐚 😈 𝐋𝐞𝐭'𝐬 𝐊𝐢𝐥𝐥 𝐁𝐚𝐝 𝐏𝐞𝐨𝐩𝐥𝐞𝐬\n𝐁𝐲 𝐖𝐫𝐢𝐭𝐢𝐧𝐠 𝐓𝐡𝐨𝐬𝐞 𝐍𝐚𝐦𝐞 𝐎𝐧 𝐃𝐞𝐚𝐭𝐡 𝐍𝐨𝐭𝐞 ❤======")      
 # for test purposes
 def error_callback(update: Update, context: CallbackContext):
     error = context.error
