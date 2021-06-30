e995081fc8e412639770ea80b566d54fffc57d13
LEGEND_OP <84610766+LEGEND-Xxxxx@users.noreply.github.com>
2021-06-30 11:25:38 +0530

    Update alive.py

--- LightYagami/modules/alive.py
+++ LightYagami/modules/alive.py
@@ -22,11 +22,11 @@ async def awake(event):
   LEGENDX = "𝐇𝐄𝐘𝐀𝐀 𝐈'𝐌 𝐋𝐈𝐆𝐇𝐓 𝐘𝐀𝐆𝐀𝐌𝐈 😈\n\n"
   LEGENDX += "𝐀𝐋𝐋 𝐒𝐘𝐒𝐓𝐄𝐌 𝐖𝐎𝐑𝐊𝐈𝐍𝐆 𝐏𝐑𝐎𝐏𝐄𝐑𝐋𝐘\n\n"
   LEGENDX += "𝐋𝐈𝐆𝐇𝐓 𝐘𝐀𝐆𝐀𝐌𝐈 𝐎𝐒 : 𝟑. 𝟖 𝐋𝐄𝐓𝐄𝐒𝐓\n\n"
-  LEGENDX += f"𝐌𝐘 𝐌𝐀𝐒𝐓𝐄𝐑 𓆩𝗥𝗢𝗖𝗞𝗬𓆪\n\n"
+  LEGENDX += f"𝐌𝐘 𝐌𝐀𝐒𝐓𝐄𝐑 𓆩𝗗𝗘𝗩𝗜𝗟𓆪\n\n"
   LEGENDX += "𝐅𝐔𝐋𝐋𝐘 𝐔𝐏𝐃𝐀𝐓𝐄𝐃\n\n"
   LEGENDX += "𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 : 𝟏. 𝟏𝟗. 𝟓 𝐋𝐀𝐓𝐄𝐒𝐓\n\n"
   LEGENDX += "𝐓𝐇𝐀𝐍𝐊𝐒 𝐅𝐎𝐑 𝐀𝐃𝐃𝐈𝐍𝐆 𝐌𝐄 𝐇𝐄𝐑𝐄"
-  BUTTON = [[Button.url("𝐌𝐀𝐒𝐓𝐄𝐑", "https://t.me/ROCKY_ALONE_FIGHTER"), Button.url("𝐃𝐄𝐕𝐋𝐎𝐏𝐄𝐑", "https://t.me/DANISH_ALONE_FIGHTER")]]
+  BUTTON = [[Button.url("𝐌𝐀𝐒𝐓𝐄𝐑", "https://t.me/ALONE_SHADOW_X"), Button.url("𝐃𝐄𝐕𝐋𝐎𝐏𝐄𝐑", "https://t.me/ITZ_DEVIL_OP")]]
   BUTTON +=[[Button.url("𝐔𝐏𝐃𝐀𝐓𝐄𝐒", "https://t.me/KIRAUPDATESS"), Button.url("𝐒𝐔𝐏𝐏𝐎𝐑𝐓", "https://t.me/KIRASUPPORT")]]
   BUTTON += [[custom.Button.inline(" 𝐈'𝐌 𝐊𝐈𝐑𝐀 😈", data="LEGENDX")]]
   await LightYagami.send_file(event.chat_id, PHOTO, caption=LEGENDX,  buttons=BUTTON)
